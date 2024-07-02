7bab470431d816b08cb2e65c2a97bec317771c3b
Stevenalv123 <stevenalv222@gmail.com>
2024-07-01 18:34:34 -0600

    fecha

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class App {
+    static LocalDate hoy = LocalDate.now();
+    static DateTimeFormatter formatofecha = DateTimeFormatter.ofPattern("EEEE, dd 'de' MMMM 'de' yyyy");
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("Pulperia Gloria");
+        System.out.println("                                                  Fecha: " + hoy.format(formatofecha));
+
     }
 }
