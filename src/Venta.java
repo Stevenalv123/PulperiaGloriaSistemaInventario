9987f130598e3dc85b3c533713f70522947e9bb1
RonaldO239 <ronald.oporta@std.eni.edu.ni>
2024-07-03 13:36:09 -0600

    'kaka'

---
+++ src/Venta.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Venta {
+    private ArrayList<VentaDetalle> historialVentas;
+
+    public Venta() {
+        this.historialVentas = new ArrayList<>();
+        
+    }
+
+    public void agregarVenta(Producto producto, int cantidad) {
+        VentaDetalle venta = new VentaDetalle(producto, cantidad);
+        this.historialVentas.add(venta);
+    }
+
+    public ArrayList<VentaDetalle> consultarHistorial() {
+        return this.historialVentas;
+    }
+
+    public double totalVentasDia() {
+        double total = 0;
+        for (VentaDetalle venta : this.historialVentas) {
+            total += venta.getTotal();
+        }
+        return total;
+    }
+}
